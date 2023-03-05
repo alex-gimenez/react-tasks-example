34b60e043357dee8add55fdfb6e6f95adb28a0cb
Alejandro Giménez <alejandro.gimenez@omron.com>
2023-03-05 20:55:48 +0100

    first commit

---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect,useState, createContext } from "react";
+import { tasks as data } from "../data/tasks";
+
+export const TaskContext = createContext();
+
+export function TaskContextProvider(props) {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  function createTask(task) {
+    setTasks([
+      ...tasks,
+      {
+        title: task.title,
+        id: tasks.length,
+        description: task.description,
+      },
+    ]);
+  }
+
+  function deleteTask(taskId) {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+    console.log(tasks);
+    console.log(taskId);
+  }
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  return (
+    <TaskContext.Provider
+      value={{
+        tasks: tasks,
+        createTask: createTask,
+        deleteTask: deleteTask,
+      }}
+    >
+      {props.children}
+    </TaskContext.Provider>
+  );
+}
